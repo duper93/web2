1a8fab68280351ed34052379a762786a44368c7b
duper93 <91032504+duper93@users.noreply.github.com>
2021-10-23 00:04:04 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+/*window.addEventListener("scroll",function () {
+    var hearder = document.querySelector("header");
+    hearder.classList.toggle("sticky",window.scrollY>0);
+    
+})*/
+
+let menuToggle = document.querySelector('.toggle');
+let navigation = document.querySelector('.navigation');
+menuToggle.onclick = function(){
+    menuToggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
+let list=document.querySelectorAll('.list');
+for(let i=0;i<list.length;i++){
+    list[i].onclick = function(){
+        let j=0;
+        while(j < list.length){
+            list[j++].className= 'list';
+        }
+        list[i].className ='list active';
+    }
+}
